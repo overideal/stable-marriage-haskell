259f017eb8fcf06e123eb9d3107fb3ab6ad14103
overideal <89090800+overideal@users.noreply.github.com>
2025-04-29 19:21:33 +0200

    Add hasFavorite and further improvements

--- src/MarriageProblem.hs
+++ src/MarriageProblem.hs
@@ -4,41 +4,47 @@
 
 module MarriageProblem where
 
-import Data.List -- For sort, findIndex
+import Data.List
 import qualified Data.Map as Map
-import Data.Maybe -- For fromJust
-import Data.Tuple.Extra -- For &&&, snd3, uncurry3
+import Data.Maybe (fromJust)
+import Data.Tuple.Extra -- (&&&), uncurry3, dupe, first, second, swap
 
+
+
+-- * Types
 
 type Man a = a
-type Woman a = a
+type Woman b = b
 
 type PrefMen a b = [(Man a,[Woman b])]
 type PrefWomen a b = [(Woman b,[Man a])]
 
 type PartialMatching a b = Map.Map (Woman b) (Man a)
 
-type Matching a b = [(Man a, Woman b)]
+type Matching a b = Map.Map (Man a) (Woman b)
 
-deferredAcceptanceAlgorithm :: (Eq a, Ord b) => PrefMen a b -> PrefWomen a b -> Matching a b
-deferredAcceptanceAlgorithm prefs_men prefs_women =
-  map swap . Map.toList . snd .
-  until (null . fst . fst) iterator $ ((men, prefs_men), Map.empty)
-  where
-    -- iterator :: (([Man a], PrefMen a b), PartialMatching a b) -> (([Man a],PrefMen a b), PartialMatching a b)
-    iterator ((m:men,prefs_men), matching) =
-      let (new_matching, new_prefs_men, new_man) =
-            eval_proposal m (get_favorite_woman m prefs_men) prefs_men prefs_women matching
-      in ((update_men new_man men, new_prefs_men), new_matching)
-    update_men :: Maybe (Man a) -> [Man a] -> [Man a]
-    update_men (Just m) men = m : men
-    update_men Nothing men = men
-    men = map fst prefs_men
+
+
+-- * Main Algorithm
+
+deferredAcceptanceAlgorithm :: (Ord a, Ord b) => PrefMen a b -> PrefWomen a b -> Matching a b
+deferredAcceptanceAlgorithm prefs_men =
+  Map.fromList . map swap . Map.toList . flip deferredAcceptanceAlgorithmWomenPropose prefs_men
 
 
 deferredAcceptanceAlgorithmWomenPropose :: (Ord a, Eq b) => PrefMen a b -> PrefWomen a b -> Matching a b
-deferredAcceptanceAlgorithmWomenPropose prefs_men =
-   map swap . flip deferredAcceptanceAlgorithm prefs_men
+deferredAcceptanceAlgorithmWomenPropose prefs_men prefs_women =
+  snd . until (null . fst . fst) iterator $ ((women, prefs_women), Map.empty)
+  where
+    -- iterator :: (([Man a], PrefMen a b), PartialMatching a b) -> (([Man a],PrefMen a b), PartialMatching a b)
+    iterator ((w:women,prefs_women), matching) =
+      let (new_matching, new_prefs_women, new_woman) =
+            eval_proposal w (get_favorite_man w prefs_women) prefs_women prefs_men matching
+      in ((update_women new_woman women, new_prefs_women), new_matching)
+    update_women :: Maybe (Man a) -> [Man a] -> [Man a]
+    update_women (Just w) women = w : women
+    update_women Nothing women = women
+    women = map fst prefs_women
 
 
 -- Evaluate the proposal that the woman receives from the man.
@@ -56,8 +62,6 @@ eval_proposal man woman prefs_men prefs_women matching =
     woman_prefers_man _ _ _ = True
     -- old_man :: Maybe (Man a)
     old_man = Map.lookup woman matching
-
-
 
 -- Return the favorite woman of the man.
 get_favorite_woman :: Eq a => Man a -> PrefMen a b -> Woman b
@@ -80,8 +84,25 @@ get_woman_pref :: Eq b => PrefWomen a b -> Woman b -> [Man a]
 get_woman_pref prefs woman = snd . fromJust . find ((==woman) . fst) $ prefs
 
 
+
+-- * Example Generation
+
+-- Find matchings for setups with n actors per gender from the deferredAcceptanceAlgorithm
+-- that agree for both men and women.
+-- Very inefficent since it checks all permutations.
+compute_fair_matchings :: Int -> [(Matching String String, PrefMen String String, PrefWomen String String)]
+compute_fair_matchings =
+  map (\((matching,_),(prefs_men, prefs_women)) -> (matching, prefs_men, prefs_women))
+  . filter (uncurry (==) . fst)
+  . map (first (uncurry deferredAcceptanceAlgorithm &&& uncurry deferredAcceptanceAlgorithmWomenPropose) . dupe) . generate_all
+
+compute_all_matchings :: Int -> [(Matching String String, PrefMen String String, PrefWomen String String)]
+compute_all_matchings =
+  map ((\(matching,(prefs_men, prefs_women)) -> (matching, prefs_men, prefs_women)) . first (uncurry deferredAcceptanceAlgorithm) . dupe) . generate_all
+
+
 -- TODO: Could save some permutations for the women
-generate_all :: Int -> [(PrefMen String String,PrefWomen String String)]
+generate_all :: Int -> [(PrefMen String String, PrefWomen String String)]
 generate_all n = (,) <$> prefs_men <*> prefs_women
   where
     men = map (("m"++) . show) [1..n]
@@ -103,58 +124,34 @@ change_list :: (a -> Bool) -> (a -> a) -> [a] -> [a]
 change_list p f = map (\a -> if p a then f a else a)
 
 
--- My main motivation for this project was to answer the following question:
--- If a matching is the result of the deferred acceptance algorithm if both men and women propose (call it "fair"),
--- then at least one person gets their first choice.
 
+-- * Checks
 
--- Find matchings for setups with n actors per gender from the deferredAcceptanceAlgorithm
--- that agree for both men and women.
--- Very inefficent since it checks all permutations.
-compute_fair_matchings :: Int -> [(Matching String String, PrefMen String String, PrefWomen String String)]
-compute_fair_matchings =
-  map (\((matching,_),(prefs_men, prefs_women)) -> (matching, prefs_men, prefs_women))
-  . filter (uncurry (==) . fst)
-  . map (first (uncurry deferredAcceptanceAlgorithm &&& uncurry deferredAcceptanceAlgorithmWomenPropose) . dupe) . generate_all
-
-compute_all_matchings :: Int -> [(Matching String String,PrefMen String String,PrefWomen String String)]
-compute_all_matchings =
-  map ((\(matching,(prefs_men, prefs_women)) -> (matching, prefs_men, prefs_women)) . first (uncurry deferredAcceptanceAlgorithm) . dupe) . generate_all
-
-
-filter_no_favorite_person :: (Eq a, Eq b) => [(Matching a b,PrefMen a b,PrefWomen a b)] -> [(Matching a b,PrefMen a b,PrefWomen a b)]
-filter_no_favorite_person = filter (uncurry3 no_favorite_person)
-
-
--- Given a matching, a preference of men and a preference of women,
--- return True if and only if no person receives their favorite partner.
-no_favorite_person :: (Eq a, Eq b) => Matching a b -> PrefMen a b -> PrefWomen a b -> Bool
-no_favorite_person matching prefs_men prefs_women = null . filter is_favorite $ matching
-  where
-    -- is_favorite :: (Man a, Woman b) -> Bool
-    is_favorite (man, woman) =
-      (get_favorite_woman man prefs_men) == woman || (get_favorite_man woman prefs_women) == man
-
--- Return the deferredAcceptanceAlgorithm matching if and only if no person receives their favorite partner.
--- Just a wrapper around no_favorite_person for convenience.
-check_for_favorite_person :: (Eq a, Ord b) => (PrefMen a b, PrefWomen a b) -> Maybe (Matching a b)
-check_for_favorite_person prefs@(prefs_men, prefs_women) =
-  if no_favorite_person matching prefs_men prefs_women
-  then Just matching
-  else Nothing
-  where matching = uncurry deferredAcceptanceAlgorithm prefs
+-- ** Check if a matching is stable
 
 -- Check whether the given matching is stable.
 isStableMatching :: (Eq a, Eq b) => PrefMen a b -> PrefWomen a b -> Matching a b -> Bool
 isStableMatching prefs_men prefs_women matching =
-  any mapper matching
+  any mapper matching_list
   where
     -- mapper :: (Man a, Woman b) -> Bool
-    mapper (m,w) = any (new_man_is_preferred m (get_partner_woman matching w)) . takeWhile (/= w) . snd . fromJust . find ((== m) . fst) $ prefs_men
+    mapper (m,w) = any (new_man_is_preferred m (get_partner_woman matching_list w)) . takeWhile (/= w) . snd . fromJust . find ((== m) . fst) $ prefs_men
     -- new_man_is_preferred :: Man a -> Man a -> Woman b -> Bool
-    new_man_is_preferred new_man old_man woman = (== new_man) . fromJust . find (`elem` [old_man,new_man]) . snd .
+    new_man_is_preferred new_man old_man woman =
+      (== new_man) . fromJust . find (`elem` [old_man,new_man]) . snd .
       fromJust . find ((== woman) . fst) $ prefs_women
+    matching_list = Map.toList matching
 
-
-get_partner_woman :: Eq b => Matching a b -> Woman b -> Man a
+get_partner_woman :: Eq b => [(Man a, Woman b)] -> Woman b -> Man a
 get_partner_woman matching w = fst . fromJust $ find ((== w) . snd) matching
+
+-- ** Check if someone gets favorite person
+
+-- Given a matching, a preference of men and a preference of women,
+-- return True if and only if no person receives their favorite partner.
+hasFavorite :: (Eq a, Eq b) => Matching a b -> PrefMen a b -> PrefWomen a b -> Bool
+hasFavorite matching prefs_men prefs_women = not . any is_favorite . Map.toList $ matching
+  where
+    -- is_favorite :: (Man a, Woman b) -> Bool
+    is_favorite (man, woman) =
+      get_favorite_woman man prefs_men == woman || get_favorite_man woman prefs_women == man
